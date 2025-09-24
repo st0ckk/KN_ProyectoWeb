24dade1fa61f06858acc40e1b2ea850aeec30764
st0ckk <anakin.salazar07@gmail.com>
2025-09-24 10:36:47 -0600

    Reposicion clase 1

--- KN_ProyectoWeb/Controllers/HomeController.cs
+++ KN_ProyectoWeb/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace KN_ProyectoWeb.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Registro()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
