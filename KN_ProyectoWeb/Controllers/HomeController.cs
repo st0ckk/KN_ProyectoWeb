8f0c97a4e363e548a404fe7e8b22d0431726c26b
st0ckk <anakin.salazar07@gmail.com>
2025-09-27 14:06:07 -0600

    Fin de semana 3

--- KN_ProyectoWeb/Controllers/HomeController.cs
+++ KN_ProyectoWeb/Controllers/HomeController.cs
@@ -1,22 +1,44 @@
-﻿using System.Web.Mvc;
+﻿using KN_ProyectoWeb.Models;
+using System.Web.Mvc;
 
 namespace KN_ProyectoWeb.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            return RedirectToAction("Principal","Home");
+        }
+
+        [HttpGet]
         public ActionResult Registro()
         {
             return View();
         }
-
+        [HttpPost]
+        public ActionResult Registro(Usuario usuario)
+        {
+            return View();
+        }
+        [HttpGet]
         public ActionResult RecuperarAcceso()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario usuario)
+        {
+            return View();
+            /* validar que el usuario exista
+             generar clave temp
+            enviar clave temp*/
         }
         public ActionResult Principal()
         {
---
+++ KN_ProyectoWeb/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace KN_ProyectoWeb.Models
+{
+    public class Usuario
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contrasenia { get; set; }
+
+    }
+}
